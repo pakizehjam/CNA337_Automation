ebf2c3ef89164b28c6cb86ad42b0c92312ba2d70
Mohammad Pakizeh Jam <61758931+pakizehjam@users.noreply.github.com>
2020-11-29 20:03:56 -0800

    Ping Automation
    I changed the main code.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # This is the template code for the CNA337 Final Project
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
-
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
-
+    print("Server Automator v0.1 by mohammad Pakizeh jam")
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    import os
+    name = "13.58.93.136"
+    response = os.system("ping -n 4 " + name)
     # TODO - Call Ping method and print the results
+    if response == 0:
+        print(name, 'is up!')
+    else:
+        print(name, 'is down!')
